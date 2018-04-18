865d7a47a23f363b96be33a0f0e6406c657b01fd
Elvin Agaragimow <imejibuh@gmail.com>
2018-04-18 17:34:20 +0300

    Обработки форм: начало. (пустой шаблон формы)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  headerText = 'Сервисы .';
+  headerText = 'Работа с формами.';
+
+  // @ViewChild('form') form: NgForm;    // Есть такой вариант
+
+  answers = [{
+    type: 'yes',
+    text: 'Да'
+  }, {
+    type: 'no',
+    text: 'нет'
+  }];
 
   constructor() {
   }
 
   ngOnInit() {
   }
+
+  submitForm(form: NgForm) {
+    console.log(form.value);
+  }
+
 }
