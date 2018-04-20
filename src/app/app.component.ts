57b7d3b627166584aa29cbbb2c141948698d9c80
Elvin Agaragimow <imejibuh@gmail.com>
2018-04-20 13:05:09 +0300

    Clear project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,17 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  headerText = 'Работа с формами.';
-
-  answers = [{
-    type: 'yes',
-    text: 'Да'
-  }, {
-    type: 'no',
-    text: 'нет'
-  }];
-
-  form: FormGroup;
-  charsCount = 7;
+  headerText = 'Работа с HTTP запросами.';
 
 
   constructor() {
   }
 
   ngOnInit() {
-    this.form = new FormGroup({
-      user: new FormGroup({
-        email: new FormControl('', [
-          Validators.email,
-          Validators.required
-        ], this.checkForEmail),
-        password: new FormControl('', [
-          Validators.required,
-          this.chekForlength.bind(this) // bind(this) задаёт область видимости нашего класса
-        ]),
-      }),
-      country: new FormControl('ua', Validators.required),
-      answer: new FormControl('yes', Validators.required)
-    });
-  }
-
-  // my validator
-  chekForlength(control: FormControl) {
-    if (control.value.length <= (this.charsCount - 1)) {
-      return {
-        'lengthError': true
-      };
-    }
-    return null;
-  }
-
-  checkForEmail(control: FormControl): Promise<any> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@mail.ru') {
-          resolve({
-            'emailIsUsed': true
-          });
-        } else {
-          resolve(null);
-        }
-      }, 2500);
-    });
-  }
-
-  onSubmit() {
-    console.log('Valid: ' + this.form.valid);
-    console.log(this.form.controls);
   }
 
 }
