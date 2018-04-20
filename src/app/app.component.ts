e60cbfa842d7c6eb4e08d8985eb5707cf6a58eaa
Elvin Agaragimow <imejibuh@gmail.com>
2018-04-20 13:52:02 +0300

    HTTP. Установка JSON-сервера. Add db.json file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   headerText = 'Работа с HTTP запросами.';
-
+  // =======================================
+  cars = [];
 
   constructor() {
   }
