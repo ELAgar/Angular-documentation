8622b0465b4bc02c1bf78497b1df37cd727ea8ab
Elvin Agaragimow <imejibuh@gmail.com>
2018-04-25 12:45:42 +0300

    Testing. Testing component. Basic.

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
+  isCarVisible = true;
+  title = 'My car header';
 
   constructor() { }
 
