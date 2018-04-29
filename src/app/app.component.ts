09e623941a5c4a9bf8ba828355e002df92dd7a09
Elvin Agaragimow <imejibuh@gmail.com>
2018-04-29 19:50:35 +0300

    Animations. Анимация на обьект которого нет в Доме.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,19 @@
 import {Component, OnInit} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {divTrigger} from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [
-    trigger('clickedDiv', [
-      state('start', style({
-        backgroundColor: 'blue',
-        width: '200px',
-        height: '200px',
-        margin: '50px'
-      })),
-      state('end', style({
-        backgroundColor: 'red',
-        width: '300px',
-        height: '300px',
-        margin: '50px'
-      })),
-      state('active', style({
-        backgroundColor: 'red',
-        width: '170px',
-        height: '170px',
-        margin: '50px'
-      })),
-      transition('start <=> end', animate('800ms 0.5s ease-in-out')),
-      transition('start => active', animate(400)),
-      transition('active => end', animate(1000))
-    ]),
-    trigger('multi', [
-      state('start', style({
-        width: '150px',
-        height: '150px',
-        border: '5px solid silver'
-      })),
-      state('end', style({
-        width: '170px',
-        height: '170px',
-        background: 'skyblue'
-      })),
-      transition('start <=> end', [
-        style({
-          background: 'red' // сразу красный
-        }),
-        animate(1500, style({
-          background: 'yellow' // переход за 1.5 сек в желтый
-        })),
-        animate(1000, style({
-          background: 'green', // переход за сек в зелёный
-          width: '200px',
-          height: '200px',
-        })),
-        animate(1000) // переход за сек в сосотояние end, в синий
-      ])
-    ])
-  ]
+  animations: [divTrigger]
 })
 export class AppComponent implements OnInit {
   title = 'Работа с анимацией.';
-  clickedDivState = 'start';
-  multiState = 'start';
+  isVisible = false;
 
   constructor() {
   }
 
   ngOnInit() {
-  }
-
-  changeDivState() {
-    this.clickedDivState = 'end';
-    setTimeout(() => {
-      this.clickedDivState = 'start';
-    }, 2000);
   }
 
 }
